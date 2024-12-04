df525b0b281acb127025bd2aa31bc5de8fc84189
Medjit Saitov <saitovmedjit@gmail.com>
2024-12-04 21:48:07 +0200

    Removed log cuntions.

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const mp3Duration = require('mp3-duration');
 
 const app = express();
 const PORT = 3333;
-const MAX_LOG_SIZE = 50 * 1024 * 1024; // 50 MB
-
-let logBuffer = [];
-let logSize = 0;
 
 const server = app.listen(PORT, listening);
 
@@ -21,58 +17,12 @@ function listening() {
   console.log(`Server is running at http://localhost:${PORT}`);
 }
 
-function logRequest(req, res, next) {
-  let source;
-  const cfConnectingIp = req.headers['cf-connecting-ip'];
-
-  if (cfConnectingIp) {
-    source = 'Cloudflare Tunnel';
-  } else if (req.ip.startsWith('127.') || req.ip.startsWith('::1')) {
-    source = 'Local';
-  } else {
-    source = 'External';
-  }
-
-  const logEntry = {
-    date: new Date().toISOString(),
-    ip: cfConnectingIp || req.ip,
-    source: source,
-    method: req.method,
-    url: req.originalUrl
-  };
-
-  const logEntryString = JSON.stringify(logEntry) + os.EOL;
-  const logEntrySize = Buffer.byteLength(logEntryString);
-
-  // Add the log entry if it doesn't exceed the max size
-  if (logSize + logEntrySize <= MAX_LOG_SIZE) {
-    logBuffer.push(logEntryString);
-    logSize += logEntrySize;
-  } else {
-    // If adding the new log entry exceeds the max size, clear the log buffer
-    console.log("Log buffer exceeded 50MB, clearing log buffer...");
-    logBuffer = [];
-    logSize = 0;
-  }
-
-  next();
-}
-
-// Middleware to log every request
-app.use(logRequest);
-
 // Serve static files and directory listings for /data
 app.use('/media_files', express.static(path.join(__dirname, 'media_files')));
 app.use('/media_files', serveIndex(path.join(__dirname, 'media_files'), { 'icons': true }));
 
 // Serve index page from the data directory at the root URL
 app.use(express.static(path.join(__dirname, 'frontend')));
-
-// Route to print logs in the browser
-app.get('/get_log', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(logBuffer.join(''));
-});
 
 //=============================== EsSelqm new frontend functions =================================
 function getDuration(filePath) {
