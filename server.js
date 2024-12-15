af90a1b5b537d3b4711591248d24ecb263292617
Medjit Saitov <saitovmedjit@gmail.com>
2024-12-15 23:34:45 +0200

    Refactor some functions for scaning files.

--- server.js
+++ server.js
@@ -25,6 +25,70 @@ app.use('/media_files', serveIndex(path.join(__dirname, 'media_files'), { 'icons
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 //=============================== EsSelqm new frontend functions =================================
+
+/**
+ * Template for an MP3 file object.
+ * 
+ * @typedef {Object} Mp3FileTemplate
+ * @property {string} name - The name of the MP3 file.
+ * @property {string} path - The file path of the MP3 file.
+ * @property {Object} data - Metadata of the MP3 file.
+ * @property {string|null} data.album - The album name.
+ * @property {string|null} data.artist - The artist name.
+ * @property {string|null} data.comment - Any comments.
+ * @property {string|null} data.partOfSet - Part of a set information.
+ * @property {string|null} data.genre - The genre of the music.
+ * @property {string|null} data.title - The title of the track.
+ * @property {string|null} data.trackNumber - The track number.
+ * @property {string|null} data.year - The release year.
+ * @property {string|null} data.image - The album cover image.
+ * @property {string} data.duration - The duration of the track.
+ * @property {string} timestamp - The timestamp of when the file was created or modified.
+ */
+const mp3FileTemplate = {
+    name: '',
+    path: '',
+    data: {
+        album: null,
+        artist: null,
+        comment: null,
+        partOfSet: null,
+        genre: null,
+        title: null,
+        trackNumber: null,
+        year: null,
+        image: null,
+        duration: ''
+    },
+    timestamp: ''
+};
+
+/**
+ * An array to store data for every scanned mp3 file.
+ * @type {Array}
+ */
+let allMp3FilesData = [];
+
+/**
+ * Indicates whether a scanning process is currently in progress.
+ * @type {boolean}
+ */
+let isScanning = false;
+
+/**
+ * @type {number}
+ * @description Tracks the count of already scanned files.
+ */
+let count = 0;
+
+
+/**
+ * Calculates the duration of an MP3 file and returns it as a formatted string.
+ *
+ * @param {string} filePath - The path to the MP3 file.
+ * @returns {Promise<string>} A promise that resolves to the duration of the MP3 file in the format "MM:SS".
+ * @throws {Error} If there is an error reading the MP3 file.
+ */
 function getDuration(filePath) {
     return new Promise((resolve, reject) => {
         mp3Duration(filePath, (err, duration) => {
@@ -41,6 +105,26 @@ function getDuration(filePath) {
     });
 }
 
+/**
+ * Retrieves metadata from an MP3 file.
+ *
+ * @param {string} filePath - The path to the MP3 file.
+ * @returns {Promise<Object>} A promise that resolves to an object containing the MP3 metadata.
+ * @property {string} name - The name of the MP3 file.
+ * @property {string} path - The relative path of the MP3 file.
+ * @property {Object} data - The metadata of the MP3 file.
+ * @property {string|null} data.album - The album name.
+ * @property {string|null} data.artist - The artist name.
+ * @property {string|null} data.comment - Any comments.
+ * @property {string|null} data.partOfSet - The part of set information.
+ * @property {string|null} data.genre - The genre of the music.
+ * @property {string|null} data.title - The title of the track.
+ * @property {string|null} data.trackNumber - The track number.
+ * @property {string|null} data.year - The year of release.
+ * @property {string|null} data.image - The album cover image in base64 format.
+ * @property {string} data.duration - The duration of the track.
+ * @property {string} timestamp - The timestamp when the metadata was retrieved.
+ */
 function getMp3Data(filePath) {
     return new Promise((resolve, reject) => {
         id3.read(filePath, async (err, tags) => {
@@ -52,16 +136,22 @@ function getMp3Data(filePath) {
                 const durationString = await getDuration(filePath);
 
                 const mp3Data = {
-                    album: tags.album || null,
-                    artist: tags.artist || null,
-                    comment: tags.comment || null,
-                    partOfSet: tags.partOfSet || null,
-                    genre: tags.genre || null,
-                    title: tags.title || null,
-                    trackNumber: tags.trackNumber || null,
-                    year: tags.year || null,
-                    image: tags.image ? tags.image.imageBuffer.toString('base64') : null,
-                    duration: durationString
+                    ...mp3FileTemplate,
+                    name: path.basename(filePath),
+                    path: filePath.replace(__dirname, '').replace(/\\/g, '/'),
+                    data: {
+                        album: tags.album || null,
+                        artist: tags.artist || null,
+                        comment: tags.comment || null,
+                        partOfSet: tags.partOfSet || null,
+                        genre: tags.genre || null,
+                        title: tags.title || null,
+                        trackNumber: tags.trackNumber || null,
+                        year: tags.year || null,
+                        image: tags.image ? tags.image.imageBuffer.toString('base64') : null,
+                        duration: durationString
+                    },
+                    timestamp: new Date().toISOString()
                 };
 
                 resolve(mp3Data);
@@ -72,31 +162,40 @@ function getMp3Data(filePath) {
     });
 }
 
-let allMp3FilesData = [];
-let isScanning = false;
-let count = 0;
 
-function updateMp3FileData(name, filePath, mp3Data) {
-    const existingFileIndex = allMp3FilesData.findIndex(file => file.path === filePath.replace(__dirname, '').replace(/\\/g, '/'));
-    if (existingFileIndex === -1) {
-        allMp3FilesData.push({
-            name: name,
-            path: filePath.replace(__dirname, '').replace(/\\/g, '/'),
-            data: mp3Data,
-            timestamp: new Date().toISOString()
-        });
-    } else {
+
+/**
+ * Updates the MP3 file data in the global `allMp3FilesData` array.
+ * If the file already exists and its data is different, it updates the existing file data.
+ * If the file does not exist, it adds the new file data to the array.
+ *
+ * @param {Object} mp3FileData - The MP3 file data to update or add.
+ * @param {string} mp3FileData.name - The name of the MP3 file.
+ * @param {Object} mp3FileData.data - The data of the MP3 file.
+ */
+function updateMp3FileData(mp3FileData) {
+    const existingFileIndex = allMp3FilesData.findIndex(file => file.name === mp3FileData.name);
+
+    if (existingFileIndex !== -1) {
+        // Update existing file data if different
         const existingFile = allMp3FilesData[existingFileIndex];
-        if (JSON.stringify(existingFile.data) !== JSON.stringify(mp3Data)) {
-            allMp3FilesData[existingFileIndex] = {
-                ...existingFile,
-                data: mp3Data,
-                timestamp: new Date().toISOString()
-            };
+        if (JSON.stringify(existingFile.data) !== JSON.stringify(mp3FileData.data)) {
+            allMp3FilesData[existingFileIndex] = mp3FileData;
         }
+    } else {
+        // Add new file data
+        allMp3FilesData.push(mp3FileData);
     }
 }
 
+/**
+ * Recursively scans a directory for all .mp3 files and processes them incrementally.
+ *
+ * @param {string} dir - The directory to scan for .mp3 files.
+ * @returns {Promise<void>} A promise that resolves when all .mp3 files have been scanned.
+ *
+ * @throws {Error} If there is an error reading the directory or processing an .mp3 file.
+ */
 async function scanAllMp3FilesIncremental(dir) {
     const list = await fs.promises.readdir(dir, { withFileTypes: true });
     for (const file of list) {
@@ -107,8 +206,20 @@ async function scanAllMp3FilesIncremental(dir) {
             try {
                 const mp3Data = await getMp3Data(filePath);
                 count++;
-                console.log(count + " - Scanned: " + file.name);
-                updateMp3FileData(file.name, filePath, mp3Data);
+                console.log(`${count} - Scanned: ${mp3Data.name}`);
+                console.log(`\tAlbum: ${mp3Data.data.album}`);
+                console.log(`\tArtist: ${mp3Data.data.artist}`);
+                //console.log(`\tComment: ${mp3Data.data.comment}`);
+                console.log(`\tPart of Set: ${mp3Data.data.partOfSet}`);
+                //console.log(`\tGenre: ${mp3Data.data.genre}`);
+                console.log(`\tTitle: ${mp3Data.data.title}`);
+                //console.log(`\tTrack Number: ${mp3Data.data.trackNumber}`);
+                //console.log(`\tYear: ${mp3Data.data.year}`);
+                console.log(`\tImage: ${mp3Data.data.image ? 'Yes' : 'No'}`);
+                console.log(`\tDuration: ${mp3Data.data.duration}`);
+                console.log(`\tTimestamp: ${mp3Data.timestamp}`);
+                console.log('');
+                updateMp3FileData(mp3Data);
             } catch (err) {
                 console.error(`Error scanning file ${file.name}:`, err);
             }
@@ -116,6 +227,16 @@ async function scanAllMp3FilesIncremental(dir) {
     }
 }
 
+/**
+ * Asynchronously initializes and scans MP3 data incrementally.
+ * 
+ * This function checks if a scanning process is already in progress. If not, it sets the scanning flag to true and proceeds to scan all MP3 files incrementally from the specified audio directory. Once the scanning is complete, it saves the scanned MP3 data to a JSON file.
+ * 
+ * @async
+ * @function initializeMp3DataIncremental
+ * @returns {Promise<void>} A promise that resolves when the scanning and saving process is complete.
+ * @throws Will throw an error if there is an issue during the scanning process.
+ */
 async function initializeMp3DataIncremental() {
     if (isScanning) {
         console.log("Scanning is already in progress...");
@@ -138,6 +259,17 @@ async function initializeMp3DataIncremental() {
 }
 
 
+/**
+ * Asynchronously loads MP3 data from a JSON file and filters out non-existent files.
+ * The function reads the JSON file containing MP3 file metadata, verifies the existence of each file,
+ * and stores the valid data in the global `allMp3FilesData` variable.
+ * It also triggers an incremental scan of the MP3 data.
+ *
+ * @async
+ * @function loadMp3DataFromFile
+ * @returns {Promise<void>} A promise that resolves when the MP3 data has been loaded and processed.
+ * @throws Will throw an error if there is an issue reading or parsing the JSON file.
+ */
 async function loadMp3DataFromFile() {
     const dataFilePath = path.join(__dirname, 'allMp3FilesData.json');
     try {
